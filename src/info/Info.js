1f6b1b009bec30a251269680415457067aa7e5c8
Ryan Carr <ryan.g.carr@hotmail.com>
2023-05-30 20:06:28 -0400

    updated info

--- src/info/Info.js
+++ src/info/Info.js
@@ -23,118 +23,102 @@ of what each of the values mean.
  */
 
 export const info = {
-    firstName: "John",
-    lastName: "Smith",
-    initials: "js", // the example uses first and last, but feel free to use three or more if you like.
-    position: "a Full Stack Developer",
-    selfPortrait: self, // don't change this unless you want to name your self-portrait in the "img" folder something else!
-    gradient: `-webkit-linear-gradient(135deg, ${colors})`, // don't change this either
-    baseColor: colors[0],
-    miniBio: [ // these are just some "tidbits" about yourself. You can look at mine https://paytonjewell.github.io/#/ for an example if you'd like
-        {
-            emoji: '☕',
-            text: 'fueled by coffee'
-        },
-        {
-            emoji: '🌎',
-            text: 'based in the US'
-        },
-        {
-            emoji: "💼",
-            text: "Systems Engineer at Google"
-        },
-        {
-            emoji: "📧",
-            text: "johnsmith@gmail.com"
-        }
-    ],
-    socials: [
-        {
-            link: "https://facebook.com",
-            icon: 'fa fa-facebook',
-            label: 'facebook'
-        },
-        {
-            link: "https://instagram.com",
-            icon: 'fa fa-instagram',
-            label: 'instagram'
-        },
-        {
-            link: "https://github.com",
-            icon: "fa fa-github",
-            label: 'github'
-        },
-        {
-            link: "https://linkedin.com",
-            icon: "fa fa-linkedin",
-            label: 'linkedin'
-        },
-        {
-            link: "https://twitter.com",
-            icon: "fa fa-twitter",
-            label: 'twitter'
-        }
-// Feel free to remove any of these that you don't have. I'll write an FAQ on how to add new ones later, let me know if you have a request for an icon!
-// Just change the links so that they lead to your social profiles.
+  firstName: 'Ryan',
+  lastName: 'Carr',
+  initials: 'rc', // the example uses first and last, but feel free to use three or more if you like.
+  position: 'a Full Stack Developer',
+  selfPortrait: self, // don't change this unless you want to name your self-portrait in the "img" folder something else!
+  gradient: `-webkit-linear-gradient(135deg, ${colors})`, // don't change this either
+  baseColor: colors[0],
+  miniBio: [
+    // these are just some "tidbits" about yourself. You can look at mine https://paytonjewell.github.io/#/ for an example if you'd like
+    {
+      emoji: '🐺',
+      text: 'Dog lover',
+    },
+    {
+      emoji: '🌎',
+      text: 'Based in the Ontario, Canada',
+    },
+    {
+      emoji: '⛺️',
+      text: 'Camping Enthusiast',
+    },
+    {
+      emoji: '📧',
+      text: 'ryan.g.carr@hotmail.com',
+    },
+  ],
+  socials: [
+    {
+      link: 'https://github.com/RCarr94',
+      icon: 'fa fa-github',
+      label: 'github',
+    },
+    {
+      link: 'https://www.linkedin.com/in/ryancarr201994/',
+      icon: 'fa fa-linkedin',
+      label: 'linkedin',
+    },
 
-    ],
-    bio: "Hello! I'm John. I'm a systems engineer for Google. I studied CompSci at Harvard, I enjoy long walks on the beach, and I believe artificial intelligence will inevitably rule us all one day. You should hire me!",
-    skills:
-        {
-            proficientWith: ['javascript', 'react', 'git', 'github', 'bootstrap', 'html5', 'css3', 'figma'],
-            exposedTo: ['nodejs', 'python', 'adobe illustrator']
-        }
-    ,
-    hobbies: [
-        {
-            label: 'reading',
-            emoji: '📖'
-        },
-        {
-            label: 'theater',
-            emoji: '🎭'
-        },
-        {
-            label: 'movies',
-            emoji: '🎥'
-        },
-        {
-            label: 'cooking',
-            emoji: '🌶'
-        }
-// Same as above, change the emojis to match / relate to your hobbies or interests.
-// You can also remove the emojis if you'd like, I just think they look cute :P
-    ],
-    portfolio: [ // This is where your portfolio projects will be detailed
-        {
-            title: "Project 1",
-            live: "https://paytonpierce.dev", //this should be a link to the live version of your project, think github pages, netlify, heroku, etc. Or your own domain, if you have it.
-            source: "https://github.com/paytonjewell", // this should be a link to the **repository** of the project, where the code is hosted.
-            image: mock1
-        },
-        {
-            title: "Project 2",
-            live: "https://paytonpierce.dev",
-            source: "https://github.com/paytonjewell",
-            image: mock2
-        },
-        {
-            title: "Project 3",
-            live: "https://paytonpierce.dev",
-            source: "https://github.com/paytonjewell",
-            image: mock3
-        },
-        {
-            title: "Project 4",
-            live: "https://paytonpierce.dev",
-            source: "https://github.com/paytonjewell",
-            image: mock4
-        },
-        {
-            title: "Project 5",
-            live: "https://paytonpierce.dev",
-            source: "https://github.com/paytonjewell",
-            image: mock5
-        }
-    ]
-}
+    // Feel free to remove any of these that you don't have. I'll write an FAQ on how to add new ones later, let me know if you have a request for an icon!
+    // Just change the links so that they lead to your social profiles.
+  ],
+  bio: "Hello I'm Ryan! I have a background in tech sales and management, bringing a unique perspective to the table. Problem solving has always been my passion, and I'm excited to bring that passion to the world of software development.",
+  skills: {
+    proficientWith: ['javascript', 'react', 'git', 'github', 'bootstrap', 'html5', 'css3', 'express', 'nodejs', 'python', 'django', 'postgreSQL', 'mongoose', 'mongoDB'],
+    exposedTo: ['ruby', 'bulma', 'materialize'],
+  },
+  hobbies: [
+    {
+      label: 'reading',
+      emoji: '📖',
+    },
+    {
+      label: 'hiking/camping',
+      emoji: '🏕️',
+    },
+    {
+      label: 'golfing',
+      emoji: '⛳️',
+    },
+    {
+      label: 'Formula 1',
+      emoji: '🏎️',
+    },
+
+  ],
+  portfolio: [
+    // This is where your portfolio projects will be detailed
+    {
+      title: 'Project 1',
+      live: 'https://paytonpierce.dev', //this should be a link to the live version of your project, think github pages, netlify, heroku, etc. Or your own domain, if you have it.
+      source: 'https://github.com/paytonjewell', // this should be a link to the **repository** of the project, where the code is hosted.
+      image: mock1,
+    },
+    {
+      title: 'Project 2',
+      live: 'https://paytonpierce.dev',
+      source: 'https://github.com/paytonjewell',
+      image: mock2,
+    },
+    {
+      title: 'Project 3',
+      live: 'https://paytonpierce.dev',
+      source: 'https://github.com/paytonjewell',
+      image: mock3,
+    },
+    {
+      title: 'Project 4',
+      live: 'https://paytonpierce.dev',
+      source: 'https://github.com/paytonjewell',
+      image: mock4,
+    },
+    {
+      title: 'Project 5',
+      live: 'https://paytonpierce.dev',
+      source: 'https://github.com/paytonjewell',
+      image: mock5,
+    },
+  ],
+};
